d34b71da3cd85274b24c06a2f8c484651bc5b325
Danish <dan71ish@gmail.com>
2024-06-25 11:44:03 +0530

    enhancement in about section

--- src/components/AboutTheJob.jsx
+++ src/components/AboutTheJob.jsx
@@ -1,8 +1,8 @@
 const AboutTheJob = () => {
     return (
-        <div className="px-4 md:pl-20 py-4 md:py-8 border-b">
+        <div className="px-4 md:pl-20 py-2 md:py-4 border-b">
             <h5 className="text-gray-500 text-sm font-semibold tracking-wide">About the Job</h5>
-            <ul className="list-decimal max-w-lg px-4 mt-2 text-[#3D3D3D] font-medium text-sm md:text-base">
+            <ul className="list-decimal max-w-lg px-4 mt-2 text-[#3D3D3D] font-medium text-xs md:text-base">
                 <li>Handle the UI/UX research design</li>
                 <li>
                     Work on researching on latest web applications designs & trends
@@ -14,27 +14,27 @@ const AboutTheJob = () => {
                     Work on creating graphics content and other graphic related works
                 </li>
             </ul>
-            <h5 className="text-gray-700 text-sm tracking-wide mt-2">
+            <h5 className="text-gray-500 text-sm font-semibold tracking-wide mt-2">
                 Benefits:
             </h5>
-            <ul className="list-disc px-8 text-[#3D3D3D] font-medium text-sm md:text-base">
+            <ul className="list-disc px-8 text-[#3D3D3D] font-medium text-xs md:text-base">
                 <li>
                     Health insurance
                 </li>
                 <li>Provident Fund</li>
             </ul>
 
-            <h5 className="text-gray-700 text-sm tracking-wide mt-2">
+            <h5 className="text-gray-500 text-sm tracking-wide mt-2 font-semibold">
                 Schedule:</h5>
-            <ul className="list-disc px-8 text-[#3d3d3d] font-medium text-sm md:text-base">
+            <ul className="list-disc px-8 text-[#3d3d3d] font-medium text-xs md:text-base">
                 <li>
                     Day shift
                 </li>
             </ul>
 
-            <h5 className="text-gray-700 text-sm tracking-wide mt-2">
+            <h5 className="text-gray-500 text-sm tracking-wide mt-2 font-semibold">
                 Supplemental pay types:</h5>
-            <ul className="list-disc px-8 text-[#3d3d3d] font-medium text-sm md:text-base">
+            <ul className="list-disc px-8 text-[#3d3d3d] font-medium text-xs md:text-base">
                 <li>
                     Performance bonus.
                 </li>
