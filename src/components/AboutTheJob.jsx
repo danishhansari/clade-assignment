216532b6d49c92e26f1754d9b5685619460e4ac9
Danish <dan71ish@gmail.com>
2024-06-25 11:38:41 +0530

    enhancement in about section

--- src/components/AboutTheJob.jsx
+++ src/components/AboutTheJob.jsx
@@ -1,8 +1,8 @@
 const AboutTheJob = () => {
     return (
         <div className="px-4 md:pl-20 py-4 md:py-8 border-b">
-            <h5 className="text-gray-500 text-sm tracking-wide">About the Job</h5>
-            <ul className="list-decimal max-w-lg px-4 mt-2 text-[#3D3D3D] font-medium">
+            <h5 className="text-gray-500 text-sm font-semibold tracking-wide">About the Job</h5>
+            <ul className="list-decimal max-w-lg px-4 mt-2 text-[#3D3D3D] font-medium text-sm md:text-base">
                 <li>Handle the UI/UX research design</li>
                 <li>
                     Work on researching on latest web applications designs & trends
@@ -17,7 +17,7 @@ const AboutTheJob = () => {
             <h5 className="text-gray-700 text-sm tracking-wide mt-2">
                 Benefits:
             </h5>
-            <ul className="list-disc px-8 text-[#3D3D3D] font-medium">
+            <ul className="list-disc px-8 text-[#3D3D3D] font-medium text-sm md:text-base">
                 <li>
                     Health insurance
                 </li>
@@ -26,7 +26,7 @@ const AboutTheJob = () => {
 
             <h5 className="text-gray-700 text-sm tracking-wide mt-2">
                 Schedule:</h5>
-            <ul className="list-disc px-8 text-[#3d3d3d] font-medium">
+            <ul className="list-disc px-8 text-[#3d3d3d] font-medium text-sm md:text-base">
                 <li>
                     Day shift
                 </li>
@@ -34,7 +34,7 @@ const AboutTheJob = () => {
 
             <h5 className="text-gray-700 text-sm tracking-wide mt-2">
                 Supplemental pay types:</h5>
-            <ul className="list-disc px-8 text-[#3d3d3d] font-medium">
+            <ul className="list-disc px-8 text-[#3d3d3d] font-medium text-sm md:text-base">
                 <li>
                     Performance bonus.
                 </li>
