013e8a2063000de9e5ca7bd2b602352f9fed5e4f
Danish <dan71ish@gmail.com>
2024-06-25 16:00:48 +0530

    refactoring

--- src/components/JobAction.jsx
+++ src/components/JobAction.jsx
@@ -3,8 +3,8 @@ const JobAction = () => {
     return (
         <div className="py-4 md:py-6 md:pl-6 max-w-sm px-6">
             <div className="flex items-center gap-4">
-                <button className="px-4 md:px-7 py-2 border rounded-lg border-[#dc4a2d] text-[#dc4a2d] bg-[#FEF4F2] tracking-wider text-sm md:text-base shadow-md font-semibold"><Trash2 className='w-4 inline-block mr-2' />Delete job</button>
-                <button className="px-4 md:px-7 py-2 shadow-inner drop-shadow-md rounded-lg bg-[#dc4a2d] text-white border text-sm md:text-base md:font-semibold"><Pencil className="w-4 inline-block mr-2" />Edit job</button>
+                <button className="px-2 md:px-7 py-2 border rounded-lg border-[#dc4a2d] text-[#dc4a2d] bg-[#FEF4F2] tracking-wider text-base shadow-md font-semibold"><Trash2 className='w-4 inline-block mr-2' />Delete job</button>
+                <button className="px-2 md:px-7 py-2 shadow-inner drop-shadow-md rounded-lg bg-[#dc4a2d] text-white border text-base md:font-semibold"><Pencil className="w-4 inline-block mr-2" />Edit job</button>
             </div>
             <div className="flex flex-col mt-8 gap-4">
                 <div className="flex py-4 border-b justify-between pr-2">
