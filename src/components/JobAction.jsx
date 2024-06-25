c63852ddd3de9def40874e9e179434554fc6b4d2
Danish <dan71ish@gmail.com>
2024-06-25 12:59:03 +0530

    jobaction

--- src/components/JobAction.jsx
+++ src/components/JobAction.jsx
@@ -1,26 +1,27 @@
+import { Trash2, Pencil, Users, UserRoundCheck, Eye, MessageSquare } from 'lucide-react'
 const JobAction = () => {
     return (
-        <div>
-            <div className="flex items-center">
-                <button>Delete job</button>
-                <button>Edit job</button>
+        <div className="py-4 md:py-6 md:pl-6 max-w-sm pr-6">
+            <div className="flex items-center gap-4">
+                <button className="px-7 py-2 border rounded-lg border-[#dc4a2d] text-[#dc4a2d] bg-[#FEF4F2] tracking-wider"><Trash2 className='w-4 inline-block mr-2' />Delete job</button>
+                <button className="px-7 py-2 shadow-inner drop-shadow-md rounded-lg bg-[#dc4a2d] text-white border "><Pencil className="w-4 inline-block mr-2" />Edit job</button>
             </div>
-            <div className="flex flex-col">
-                <div className="flex">
-                    <p>Applicants</p>
-                    <p>400</p>
+            <div className="flex flex-col mt-8 gap-4">
+                <div className="flex py-4 border-b justify-between pr-2">
+                    <p className='text-gray-500 font-medium tracking-wide'><Users className='inline-block mr-2' />Applicants</p>
+                    <p className='text-lg text-gray-500 font-semibold tracking-wider'>400</p>
                 </div>
-                <div className="flex">
-                    <p>Matches</p>
-                    <p>100</p>
+                <div className="flex py-4 border-b justify-between pr-2">
+                    <p className='text-gray-500 font-medium tracking-wide'><UserRoundCheck className='inline-block mr-2' />Matches</p>
+                    <p className='text-lg text-gray-500 font-semibold tracking-wider'>100</p>
                 </div>
-                <div className="flex">
-                    <p>Messages</p>
-                    <p>147</p>
+                <div className="flex py-4 border-b justify-between pr-2">
+                    <p className='text-gray-500 font-medium tracking-wide'><MessageSquare className='inline-block mr-2' />Messages</p>
+                    <p className='text-lg text-gray-500 font-semibold tracking-wider'>147</p>
                 </div>
-                <div className="flex">
-                    <p>Views</p>
-                    <p>800</p>
+                <div className="flex py-4 justify-between pr-2">
+                    <p className='text-gray-500 font-medium tracking-wide'><Eye className='inline-block mr-2' />Views</p>
+                    <p className='text-lg text-gray-500 font-semibold tracking-wider'>800</p>
                 </div>
             </div>
         </div>
--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -7,15 +7,17 @@ import Quote from "./Quote"
 
 const MainLayout = () => {
   return (
-    <div>
-      <div className="py-4 md:py-6">
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div className="py-4 md:py-6 col-span-2 border-r w-full">
         <JobTitle />
         <JobRequirement />
         <AboutTheJob />
         <AboutTheCompany />
       </div>
-      {/* <JobAction /> */ }
-      {/* <Quote /> */ }
+      <div className="grid-cols-1 md:mx-auto border-r">
+        <JobAction />
+        {/* <Quote /> */ }
+      </div>
     </div>
   )
 }
